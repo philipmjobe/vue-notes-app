48527cc7d36111d89e39909bba988d29d4917ec2
Philip <philipmjobe@gmail.com>
2021-11-22 14:12:20 -0500

    added noteselector.vue, noteselectors.vue, and noteeditor.vue

---
+++ src/components/NoteSelector.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="note-selector" v-for="note in notes" :key="note.id">
+      <p class="note-selector-title">{{ note.title }}</p>
+      <p class="note-selector-timestamp">{{ note.timestamp }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      notes: [
+        { id: 1, title: "First note...", timestamp: "Timestamp here.." },
+        { id: 2, title: "Seconde note...", timestamp: "Timestamp here.." },
+        { id: 3, title: "Third note...", timestamp: "Timestamp here.." },
+      ],
+    };
+  },
+};
+</script>
